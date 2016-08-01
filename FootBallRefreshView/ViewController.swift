d69c80c9f4e1ac3b5fff8660bd2f0f17f7d2e3b0
RG <ieppeo@163.com>
2016-08-01 21:20:37 +0800

    Initial Commit

---
+++ FootBallRefreshView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FootBallRefreshView
+//
+//  Created by 武飞跃 on 2016/8/1.
+//  Copyright © 2016年 RG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
